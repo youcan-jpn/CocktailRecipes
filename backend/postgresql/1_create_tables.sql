94780e72a6af56955b55406cb366f2f25d6b32ba
yuta ono <youcana40627@gmail.com>
2022-02-20 01:23:29 +0900

    add postgresql init files

---
+++ backend/postgresql/1_create_tables.sql
@@ -0,0 +1,48 @@
+-- Postgresql
+
+CREATE USER docker;
+
+CREATE DATABASE docker;
+
+GRANT ALL PRIVILEGES ON DATABASE docker TO docker;
+
+-- change database
+\c docker
+
+-- TABLE
+CREATE TABLE IF NOT EXISTS cocktails
+(
+    cocktail_id         SERIAL      NOT NULL,
+    cocktali_name_jp    CHAR(16)    NOT NULL,
+    cocktali_name_en    CHAR(32),
+    image_url           TEXT,
+    cocktail_note_jp    TEXT        NOT NULL,
+    cocktail_note_en    TEXT,
+    PRIMARY KEY (cocktail_id)
+);
+
+CREATE TABLE IF NOT EXISTS recipes
+(
+    cocktail_id         INT         REFERENCES cocktails(cocktail_id)   NOT NULL,
+    order               INT         NOT NULL,
+    recipe_jp           TEXT        NOT NULL,
+    recipe_en           TEXT,
+    PRIMARY KEY (cocktail_id, order)
+);
+
+CREATE TABLE IF NOT EXISTS ingredients
+(
+    ingredient_id       SERIAL      NOT NULL,
+    ingredient_name_jp  CHAR(32)    NOT NULL,
+    ingredient_name_en  CHAR(64),
+    PRIMARY KEY (ingredient_id)
+);
+
+CREATE TABLE IF NOT EXISTS cocktail_materials
+(
+    cocktail_id         INT         REFERENCES cocktails(cocktail_id)       NOT NULL,
+    ingredient_id       INT         REFERENCES ingredients(ingredient_id)   NOT NULL,
+    amount_jp           VARCHAR(32) NOT NULL,
+    amount_en           VARCHAR(100),
+    PRIMARY KEY (cocktail_id, ingredient_id)
+);
---
+++ backend/postgresql/2_insert_samples.sql
@@ -0,0 +1,19 @@
+\c docker
+
+-- sample cocktail
+INSERT INTO cocktails(cocktali_name_jp, cocktali_name_en, image_url, cocktail_note_jp, cocktail_note_en) VALUES ("ジントニック", "gin tonic", "", "17世紀にイギリス東インド会社の社員が、赴任中のインドでマラリア対策に飲んだ飲料が原型とされる。ベースを替えた、ラム・トニック、テキーラ・トニック（テコニック）、ウオツカ・トニックも飲まれている", "Classic and easy, the gin and tonic (or G&T) is light and refreshing. It's a simple mixed drink that requires just the two named ingredients and a hint of lime, all of which are natural flavor companions. This is a great choice for happy hour, dinner, or anytime you simply want an invigorating beverage.");
+
+-- sample ingredients
+INSERT INTO ingredients(ingredient_name_jp, ingredient_name_en) VALUES ("ジン", "gin");
+INSERT INTO ingredients(ingredient_name_jp, ingredient_name_en) VALUES ("トニックウォーター", "tonic water");
+INSERT INTO ingredients(ingredient_name_jp, ingredient_name_en) VALUES ("ライム", "lime");
+
+-- sample amount
+INSERT INTO cocktail_materials VALUES (1, 1, "45ml", "2oz");
+INSERT INTO cocktail_materials VALUES (1, 2, "適量", "to taste");
+INSERT INTO cocktail_materials VALUES (1, 3, "1切れ", "1slice");
+
+--sample recipes
+INSERT INTO recipes VALUES (1, 1, "氷を入れたグラスにジンを注いだあとトニックウォーターを加える。", "In a highball glass filled with ice cubes, pour the gin, then top with tonic.");
+INSERT INTO recipes VALUES (1, 2, "軽くかき混ぜる", "Gently stir to combine, but not so much so that you lose carbonation.");
+INSERT INTO recipes VALUES (1, 3, "ライムを添える", "Garnish with a lime wedge.");
