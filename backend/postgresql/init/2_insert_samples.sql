f5dc2c615ed076797f694d68fd2e0769ec150bc0
yuta ono <youcana40627@gmail.com>
2022-02-21 16:36:14 +0900

    fix a sql init file: add more ingredients

--- backend/postgresql/init/2_insert_samples.sql
+++ backend/postgresql/init/2_insert_samples.sql
@@ -10,9 +10,11 @@ INSERT INTO methods(method_name_jp, method_name_en) VALUES ('ブレンド', 'Ble
 INSERT INTO cocktails(cocktail_name_jp, cocktail_name_en, image_url, method_id, cocktail_note_jp, cocktail_note_en) VALUES ('ジントニック', 'gin tonic', 'undefined', '1', '17世紀にイギリス東インド会社の社員が、赴任中のインドでマラリア対策に飲んだ飲料が原型とされる。ベースを替えた、ラム・トニック、テキーラ・トニック（テコニック）、ウオツカ・トニックも飲まれている', 'Classic and easy, the gin and tonic (or G&T) is light and refreshing. It''s a simple mixed drink that requires just the two named ingredients and a hint of lime, all of which are natural flavor companions. This is a great choice for happy hour, dinner, or anytime you simply want an invigorating beverage.');
 
 -- sample ingredients
-INSERT INTO ingredients(ingredient_name_jp, ingredient_name_en) VALUES ('ジン', 'gin');
+INSERT INTO ingredients(ingredient_name_jp, ingredient_name_en) VALUES ('ドライジン', 'dry gin');
 INSERT INTO ingredients(ingredient_name_jp, ingredient_name_en) VALUES ('トニックウォーター', 'tonic water');
 INSERT INTO ingredients(ingredient_name_jp, ingredient_name_en) VALUES ('ライム', 'lime');
+INSERT INTO ingredients(ingredient_name_jp, ingredient_name_en) VALUES ('ドライベルモット', 'dry vermouth');
+INSERT INTO ingredients(ingredient_name_jp, ingredient_name_en) VALUES ('オリーブ', 'olive');
 
 -- sample amount
 INSERT INTO cocktail_materials VALUES (1, 1, '45ml', '2oz');
